52127f630877332ac0dd4d1af2c8b37ef5b195ee
Soo Bin Lee <soobin.lee@qut>
2022-03-22 13:20:32 +1000

    Base & NewNote() added

--- SimpleNotesApplication/Program.cs
+++ SimpleNotesApplication/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+using System.Diagnostics;
 
 namespace SimpleNotesApplication
 {
@@ -6,7 +9,85 @@ namespace SimpleNotesApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            ReadCommand();
+            Console.ReadLine();
         }
+
+        private static string NoteDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Notes\";
+
+        private static void ReadCommand()
+        {
+            Console.Write(Directory.GetDirectoryRoot(NoteDirectory));
+            string Command = Console.ReadLine();
+
+            switch (Command.ToLower()) {
+                case "new":
+                    NewNote();
+                    Main(null);
+                    break;
+                case "edit":
+                    EditNote();
+                    Main(null);
+                    break;
+                case "read":
+                    ReadNote();
+                    Main(null);
+                    break;
+                case "delete":
+                    DeleteNote();
+                    Main(null);
+                    break;
+                case "shownotes":
+                    ShowNotes();
+                    Main(null);
+                    break;
+                case "dir":
+                    NotesDirectory();
+                    Main(null);
+                    break;
+                case "cls":
+                    Console.Clear();
+                    Main(null);
+                    break;
+                case "exit":
+                    Exit();
+                    break;
+                default:
+                    CommandsAvailable();
+                    Main(null);
+                    break;
+            }
+        }
+
+        private static void NewNote()
+        {
+            Console.WriteLine("Please Enter Nots:\n");
+            string input = Console.ReadLine();
+
+            XmlWriterSettings NoteSettings = new XmlWriterSettings();
+
+            NoteSettings.CheckCharacters = false;
+            NoteSettings.ConformanceLevel = ConformanceLevel.Auto;
+            NoteSettings.Indent = true;
+
+            string FileName = DateTime.Now.ToString("dd-MM-yy") + ".xml";
+
+            using (XmlWriter NewNote = XmlWriter.Create(NoteDirectory + FileName, NoteSettings)) {
+                NewNote.WriteStartDocument();
+                NewNote.WriteStartElement("Note");
+                NewNote.WriteElementString("body", input);
+                NewNote.WriteEndElement();
+
+                NewNote.Flush();
+                NewNote.Close();
+            }
+        }
+
+        private static void EditNote()
+        {
+
+        }
+
     }
 }
